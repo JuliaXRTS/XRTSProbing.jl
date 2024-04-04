af8f104a6521adc22d58c7df0701d25382c317c8
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2024-04-04 15:39:18 +0200

    added blue style to JuliaFormatter config

--- docs/make.jl
+++ docs/make.jl
@@ -1,16 +1,16 @@
 using QEDprobing
 using Documenter
 
-DocMeta.setdocmeta!(QEDprobing, :DocTestSetup, :(using QEDprobing); recursive = true)
+DocMeta.setdocmeta!(QEDprobing, :DocTestSetup, :(using QEDprobing); recursive=true)
 
 makedocs(;
-    modules = [QEDprobing],
-    authors = "Uwe Hernandez Acosta <u.hernandez@hzdr.de",
-    sitename = "QEDprobing.jl",
-    format = Documenter.HTML(;
-        canonical = "https://qedapplications.gitlab.io/QEDprobing.jl",
-        edit_link = "main",
-        assets = String[],
+    modules=[QEDprobing],
+    authors="Uwe Hernandez Acosta <u.hernandez@hzdr.de",
+    sitename="QEDprobing.jl",
+    format=Documenter.HTML(;
+        canonical="https://qedapplications.gitlab.io/QEDprobing.jl",
+        edit_link="main",
+        assets=String[],
     ),
-    pages = ["Home" => "index.md"],
+    pages=["Home" => "index.md"],
 )

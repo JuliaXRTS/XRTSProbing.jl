dbad5103420477c8c1b81ed2135782b305fc4d61
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2024-04-05 14:41:01 +0200

    fixed doc url template

--- docs/make.jl
+++ docs/make.jl
@@ -7,7 +7,11 @@ makedocs(;
     modules=[QEDprobing],
     authors="Uwe Hernandez Acosta <u.hernandez@hzdr.de",
     sitename="QEDprobing.jl",
+    repo=Documenter.Remotes.URL(
+        "https://codebase.helmholtz.cloud/qedjl-applications/QEDprobing.jl/lblob/{commit}{path}#{line}",
+    ),
     format=Documenter.HTML(;
+        repolink="https://codebase.helmholtz.cloud/qedjl-applications/QEDprobing.jl/lblob/{commit}{path}#{line}",
         canonical="https://qedapplications.gitlab.io/QEDprobing.jl",
         edit_link="main",
         assets=String[],

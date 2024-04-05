8cf7ea764a3b36da4c1a6da84d45f34dda90be38
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2024-04-05 10:43:45 +0200

    added dummy function

--- src/QEDprobing.jl
+++ src/QEDprobing.jl
@@ -2,4 +2,8 @@ module QEDprobing
 
 # Write your package code here.
 
+function dummy_func()
+    return 1
+end
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,8 @@ using Test
 
 @testset "QEDprobing.jl" begin
     # Write your tests here.
+    
+    @testset "dummy" begin
+        @test QEDprobing.dummy_func() == 1 
+    end
 end

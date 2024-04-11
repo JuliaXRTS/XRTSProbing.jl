0a552ee75a7da84b118f817540cc878517f49366
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2024-04-11 11:58:10 +0200

    formatting

--- docs/make.jl
+++ docs/make.jl
@@ -5,12 +5,14 @@ DocMeta.setdocmeta!(QEDprobing, :DocTestSetup, :(using QEDprobing); recursive=tr
 
 makedocs(;
     modules=[QEDprobing],
-         checkdocs=:exports,
+    checkdocs=:exports,
     authors="Uwe Hernandez Acosta <u.hernandez@hzdr.de",
     sitename="QEDprobing.jl",
-    repo=Documenter.Remotes.GitLab("codebase.helmholtz.cloud","qedjl-applications","QEDprobing.jl"),
+    repo=Documenter.Remotes.GitLab(
+        "codebase.helmholtz.cloud", "qedjl-applications", "QEDprobing.jl"
+    ),
     format=Documenter.HTML(;
-                           prettyurls=get(ENV, "CI", "false") == "true",
+        prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://qedapplications.gitlab.io/QEDprobing.jl",
         #edit_link="main",
         assets=String[],
